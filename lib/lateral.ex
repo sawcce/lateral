28a3c4e9ea1c7c061f9d3c53787381048c2bba62
alexis <sawcce@proton.me>
2023-12-28 21:21:24 +0100

    basic tests with new pipeline

--- lib/lateral.ex
+++ lib/lateral.ex
@@ -1,12 +1,21 @@
 defmodule Lateral do
   def start(_type, _args) do
     {:ok, pid} =
-      Supervisor.start_link([Parser, Lateral.Macros, Processor], strategy: :one_for_one)
+      Supervisor.start_link([Parser, Lateral.Macros, Processor, Compiler], strategy: :one_for_one)
+
+    config = :filename.basedir(:user_config, "lateral")
+    if !File.exists?(config) do
+      File.mkdir(config)
+      File.mkdir(Path.join(config, "packages"))
+    end
 
     result = GenServer.call(Parser, {:parse, "./examples/test.lat"})
     IO.inspect(result)
     result2 = GenServer.call(Processor, {:process, result}, :infinity)
     IO.inspect(result2)
+    result3 = GenServer.call(Compiler, {:latex, result2})
+    File.write("examples/test.tex", result3)
+    IO.inspect(result3)
 
     {:ok, pid}
   end

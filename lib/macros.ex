68c23e69886e6089b3010aae558d20960e5bc2c0
alexis <sawcce@proton.me>
2023-12-28 21:21:56 +0100

    Separated macros

--- lib/macros.ex
+++ lib/macros.ex
@@ -11,47 +11,33 @@ defmodule Lateral.Macros do
   end
 
   @impl true
-  def handle_call({"begin", previous, tokens}, _from, state) do
-    IO.inspect(state)
-    {{:token, environment}, rest} = List.pop_at(tokens, 0)
-
-    IO.puts("begin env: #{environment}")
-
-    {:reply, {[{:new_environment, environment}], previous, rest}, state}
-  end
-
-  @impl true
-  def handle_call({"end", previous, tokens}, _from, state) do
-    {{:token, environment}, rest} = List.pop_at(tokens, 0)
-    # {last_environment, others} = List.pop_at(state, 0)
-
-    {:reply, {[{:close_environment, environment}], previous, rest}, state}
-  end
-
-  @impl true
   def handle_call({"use", previous, tokens}, _from, state) do
     {{:token, package}, rest} = List.pop_at(tokens, 0)
 
-    {exports, _} = Code.eval_file("./packages/#{package}.exs")
-    IO.inspect exports
+    if File.exists?("./packages/#{package}.exs") do
+      {exports, _} = Code.eval_file("./packages/#{package}.exs")
+      IO.inspect(exports)
 
-    IO.puts("Use: #{package}")
-    {:reply, {[], previous, rest}, Map.merge(state, exports)}
+      IO.puts("Use: #{package}")
+      {:reply, {[], previous, rest}, Map.merge(state, exports)}
+    else
+      {:reply, {[{:use_unknown, package}], previous, rest}, state}
+    end
   end
 
   @impl true
 
   def handle_call({arbitrary_macro, previous, tokens}, _from, state) do
-    IO.puts("Macro: #{arbitrary_macro}, #{inspect state}")
+    IO.puts("Macro: #{arbitrary_macro}, #{inspect(state)}")
     macro = Map.get(state, String.to_existing_atom(arbitrary_macro))
+
     if macro == nil do
-      IO.puts "UNKNOWN MACRO: #{arbitrary_macro}"
+      IO.puts("UNKNOWN MACRO: #{arbitrary_macro}")
       {:reply, {[], previous, tokens}, state}
     else
       res = apply(macro, [previous, tokens])
-      IO.puts "Res: #{inspect res}"
+      IO.puts("Res: #{inspect(res)}")
       {:reply, res, state}
     end
-
   end
 end
--- packages/defaults.exs
+++ packages/defaults.exs
@@ -1,8 +1,29 @@
 defmodule Defaults do
   def section(previous, next) do
     {section_title, rest} = Processor.Utils.parse_atom_forward(next)
-    {[{:latex_macro_invocation, [{:atom, section_title}]}], previous, rest}
+    t =  Processor.Utils.process(section_title)
+    {[{:tex_macro_invocation, "section", [{:atom, t}]}], previous, rest}
+  end
+
+  def class(previous, tokens) do
+    {{:token, document_class}, rest} = List.pop_at(tokens, 0)
+
+    {[{:tex_macro_invocation, "documentclass", [{:atom, document_class}]}], previous, rest}
+  end
+
+  def begin(previous, tokens) do
+    {{:token, environment}, rest} = List.pop_at(tokens, 0)
+
+    IO.puts("begin env: #{environment}")
+
+    {[{:tex_macro_invocation, "begin", [{:atom, environment}]}], previous, rest}
+  end
+
+  def end_(previous, tokens) do
+    {{:token, environment}, rest} = List.pop_at(tokens, 0)
+
+    {[{:tex_macro_invocation, "end", [{:atom, environment}]}], previous, rest}
   end
 end
 
-%{section: &Defaults.section/2}
+%{class: &Defaults.class/2, section: &Defaults.section/2, begin: &Defaults.begin/2, end: &Defaults.end_/2}
